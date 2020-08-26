e2de6c65e2316e3642c1b8fef5b3d389d8381266
shindelhaim <sz@sem-wolf.org>
2020-08-26 03:06:40 +0300

    refactor in the code

--- run_attention.py
+++ run_attention.py
@@ -32,21 +32,29 @@ def find_tfl_lights(c_image: np.ndarray, **kwargs):
     :param kwargs: Whatever config you want to pass in here
     :return: 4-tuple of x_red, y_red, x_green, y_green
     """
-    x = np.arange(-100, 100, 20) + c_image.shape[1] / 2
-    y_red = [c_image.shape[0] / 2 - 120] * len(x)
-    y_green = [c_image.shape[0] / 2 - 100] * len(x)
-    return x, y_red, x, y_green
+
+    red_x, red_y ,green_x, green_y = my_func(c_image)
+    marking_the_coordinates(c_image, red_x, red_y, 'red')
+    marking_the_coordinates(c_image, green_x, green_y, 'green')
+    
+    # x = np.arange(-100, 100, 20) + c_image.shape[1] / 2
+    # y_red = [c_image.shape[0] / 2 - 120] * len(x)
+    # y_green = [c_image.shape[0] / 2 - 100] * len(x)
+    return red_x, red_y ,green_x, green_y
     
 
 def show_image_and_gt(image, objs, fig_num=None):
     plt.figure(fig_num).clf()
     plt.imshow(image)
     labels = set()
+    
     if objs is not None:
+        
         for o in objs:
             poly = np.array(o['polygon'])[list(np.arange(len(o['polygon']))) + [0]]
             plt.plot(poly[:, 0], poly[:, 1], 'r', label=o['label'])
             labels.add(o['label'])
+        
         if len(labels) > 1:
             plt.legend()
 
@@ -56,8 +64,10 @@ def test_find_tfl_lights(image_path, json_path=None, fig_num=None):
     Run the attention code
     """
     image = np.array(Image.open(image_path))
+
     if json_path is None:
         objects = None
+    
     else:
         gt_data = json.load(open(json_path))
         what = ['traffic light']
@@ -115,17 +125,17 @@ def get_maximum_points(image):
 def get_lights_coordinates(image, convert_image):
     kernel = get_kernel()
     grad = sg.convolve2d(convert_image, kernel, boundary='symm', mode='same')
-    fig, (ax_orig, ax_mag) = plt.subplots(2, 1, figsize=(12, 30))
+    # fig, (ax_orig, ax_mag) = plt.subplots(2, 1, figsize=(12, 30))
     
-    ax_orig.imshow(image, cmap='gray')
-    ax_orig.set_title('Original')
+    # ax_orig.imshow(image, cmap='gray')
+    # ax_orig.set_title('Original')
     
-    ax_mag.imshow(Image.fromarray(grad), cmap='gray')
-    ax_mag.set_title('Gradient magnitude')
+    # ax_mag.imshow(Image.fromarray(grad), cmap='gray')
+    # ax_mag.set_title('Gradient magnitude')
     
     axis_x, axis_y = get_maximum_points(grad)
     
-    fig.show()
+    # fig.show()
     plt.show()
     
     return axis_x, axis_y
@@ -167,33 +177,35 @@ def main(argv=None):
     Consider looping over some images from here, so you can manually exmine the results
     Keep this functionality even after you have all system running, because you sometime want to debug/improve a module
     :param argv: In case you want to programmatically run this"""
-    
 
-    image = Image.open('berlin_000181_000019_leftImg8bit.png')
-    red_x, red_y ,green_x, green_y= my_func(image)
-    marking_the_coordinates(image.convert(), red_x, red_y, 'red')
-    marking_the_coordinates(image.convert(), green_x, green_y, 'green')
-    
 
-    # parser = argparse.ArgumentParser("Test TFL attention mechanism")
-    # parser.add_argument('-i', '--image', type=str, help='Path to an image')
-    # parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
-    # parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
-    # args = parser.parse_args(argv)
-    # default_base = '../../data'
-    # if args.dir is None:
-    #     args.dir = default_base
-    # flist = glob.glob(os.path.join(args.dir, '*_leftImg8bit.png'))
-    # for image in flist:
-    #     json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
-    #     if not os.path.exists(json_fn):
-    #         json_fn = None
-    #     test_find_tfl_lights(image, json_fn)
-    # if len(flist):
-    #     print("You should now see some images, with the ground truth marked on them. Close all to quit.")
-    # else:
-    #     print("Bad configuration?? Didn't find any picture to show")
-    # plt.show(block=True)
+    parser = argparse.ArgumentParser("Test TFL attention mechanism")
+    parser.add_argument('-i', '--image', type=str, help='Path to an image')
+    parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
+    parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
+    args = parser.parse_args(argv)
+    default_base = 'data'
+    
+    if args.dir is None:
+        args.dir = default_base
+
+    flist = glob.glob(os.path.join(args.dir, '*_leftImg8bit.png'))
+    
+    for image in flist:
+        json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
+        
+        if not os.path.exists(json_fn):
+            json_fn = None
+
+        test_find_tfl_lights(image, json_fn)
+    
+    if len(flist):
+        print("You should now see some images, with the ground truth marked on them. Close all to quit.")
+    
+    else:
+        print("Bad configuration?? Didn't find any picture to show")
+
+    plt.show(block=True)
 
 
 if __name__ == '__main__':
