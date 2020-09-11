73b05dd9672942419f172f905c2c7bd39954827f
shindelhaim <sz@sem-wolf.org>
2020-09-11 03:14:36 +0300

    add main

---
+++ SRC/main.py
@@ -0,0 +1,10 @@
+from Controller.controller import Controller
+
+
+def main():
+    controller = Controller('Model\\frames_file.pls')
+    controller.run()
+
+
+if __name__ == "__main__":
+    main()
